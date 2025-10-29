2a509094633737dd07a9ea20f98781f070cfee56
Miguel M Ocque <142752734+miguelocque@users.noreply.github.com>
2025-10-29 17:07:52 -0400

    Created initial files for Spring Boot framework
    The files contain dependencies that will be utilized for each one of the use case implementations

---
+++ UnifiedPatientManager/src/main/java/com/Eges411Team/UnifiedPatientManager/UnifiedPatientManagerApplication.java
@@ -0,0 +1,13 @@
+package com.Eges411Team.UnifiedPatientManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnifiedPatientManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnifiedPatientManagerApplication.class, args);
+	}
+
+}
---
+++ UnifiedPatientManager/src/test/java/com/Eges411Team/UnifiedPatientManager/UnifiedPatientManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Eges411Team.UnifiedPatientManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifiedPatientManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
