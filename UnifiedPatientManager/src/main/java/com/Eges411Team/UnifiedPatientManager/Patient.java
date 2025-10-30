9c6c032f162ea8e80b7b648e892bb0fffb4b0c2f
Miguel Ocque <mocque@bu.edu>
2025-10-30 15:01:58 -0400

    added more fields to patient and their methods

--- UnifiedPatientManager/src/main/java/com/Eges411Team/UnifiedPatientManager/Patient.java
+++ UnifiedPatientManager/src/main/java/com/Eges411Team/UnifiedPatientManager/Patient.java
@@ -6,17 +6,21 @@ public class Patient {
     private String name;
     private int age;
     private String gender;
+    private String dateOfBirth;
+    private String address;
+    private String phoneNumber;
 
     // create the Constructor TODO
-    public Patient(String id, String name, int age, String gender) {
+    public Patient(String id, String name, int age, String gender, String dateOfBirth, String address, String phoneNumber) {
         this.id = id;
         this.name = name;
         this.age = age;
         this.gender = gender;
+        this.dateOfBirth = dateOfBirth; 
     }
 
 
-    // create the Getters and Setters TODO
+    // ID getter and setter
     public String getId() {
         return id;
     }
@@ -25,6 +29,7 @@ public class Patient {
         this.id = id;
     }
 
+    // name getter and setter
     public String getName() {
         return name;
     }
@@ -33,6 +38,7 @@ public class Patient {
         this.name = name;
     }
 
+    // age getter and setter
     public int getAge() {
         return age;
     }
@@ -41,12 +47,40 @@ public class Patient {
         this.age = age;
     }
 
+    // gender getter and setter
     public String getGender() {
         return gender;
     }
 
     public void setGender(String gender) {
         this.gender = gender;
+    }
+
+    // date of birth getter and setter
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    // address getter and setter
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    // phone number getter and setter
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
 }
