b869a72ef00c624a1cb3c7f7f1e1e6ee8f6fe93e
Miguel Ocque <mocque@bu.edu>
2025-10-30 14:56:36 -0400

    created initial Patient class with fields, getters and setters

---
+++ UnifiedPatientManager/src/main/java/com/Eges411Team/UnifiedPatientManager/Patient.java
@@ -0,0 +1,52 @@
+package com.Eges411Team.UnifiedPatientManager;
+
+public class Patient {
+    // fields TODO
+    private String id;
+    private String name;
+    private int age;
+    private String gender;
+
+    // create the Constructor TODO
+    public Patient(String id, String name, int age, String gender) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+
+    // create the Getters and Setters TODO
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+}
