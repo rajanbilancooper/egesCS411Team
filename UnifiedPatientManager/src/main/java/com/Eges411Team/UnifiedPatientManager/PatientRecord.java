255ca5c4fb3ba09d1a4cb3380f3fbe79a5bde67f
Miguel Ocque <mocque@bu.edu>
2025-10-31 14:09:18 -0400

    Modified patient class attributes to be part of the PatientRecord class instead

--- UnifiedPatientManager/src/main/java/com/Eges411Team/UnifiedPatientManager/PatientRecord.java
+++ UnifiedPatientManager/src/main/java/com/Eges411Team/UnifiedPatientManager/PatientRecord.java
@@ -1,7 +1,52 @@
 package com.Eges411Team.UnifiedPatientManager;
 
+import java.util.List;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "patient_records")
 public class PatientRecord {
+
+    // attributes
+
+    // a patient can have many allergies
+    @OneToMany(mappedBy = "patient", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private List<Allergy> allergies;
+
+    // and many medications
+    @OneToMany(mappedBy = "patient", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private List<Medication> medications;
+
+    // and prescriptions
+    @OneToMany(mappedBy = "patient", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private List<Prescription> prescriptions; 
+
+
     public PatientRecord() {
         // no params
+    }
+
+    // getter and setter for allergies
+    public List<Allergy> getAllergies() {
+        return allergies;
+    }
+    public void setAllergies(List<Allergy> allergies) {
+        this.allergies = allergies;
+    }
+
+    // getter and setter for medications
+    public List<Medication> getMedications() {
+        return medications;
+    }
+    public void setMedications(List<Medication> medications) {
+        this.medications = medications;
+    }
+    // getter and setter for prescriptions
+    public List<Prescription> getPrescriptions() {
+        return prescriptions;
+    }
+    public void setPrescriptions(List<Prescription> prescriptions) {
+        this.prescriptions = prescriptions;
     }
 }
