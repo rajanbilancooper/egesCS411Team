88b3b03eafda676bd3033678683842385cf92f9a
Miguel Ocque <mocque@bu.edu>
2025-10-31 16:15:32 -0400

    created a controller subfolder to hold all of the controllers

---
+++ UnifiedPatientManager/src/main/java/com/Eges411Team/UnifiedPatientManager/controller/PatientController.java
@@ -0,0 +1,16 @@
+package com.Eges411Team.UnifiedPatientManager.controller;
+
+import org.springframework.stereotype.Controller;
+
+import jakarta.persistence.*;
+
+@Controller
+public class PatientController {
+    
+    // the controller has no attributes for now
+    public PatientController() {
+        // no params for a controller since it just manages requests
+    }
+
+    
+}
