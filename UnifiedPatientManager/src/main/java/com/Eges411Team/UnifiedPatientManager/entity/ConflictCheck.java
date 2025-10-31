3bd988b8ee498166bda48be39e49ca4d787c9ce0
Miguel Ocque <mocque@bu.edu>
2025-10-31 16:04:39 -0400

    moved more entities into the folder, and created constructors for some of the classes

---
+++ UnifiedPatientManager/src/main/java/com/Eges411Team/UnifiedPatientManager/entity/ConflictCheck.java
@@ -0,0 +1,13 @@
+package com.Eges411Team.UnifiedPatientManager.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "conflict_checks")
+public class ConflictCheck {
+
+    // empty constructor required by JPA
+    public ConflictCheck() {
+        // no params
+    }
+}
