70911b282b48662be2b3dc61811ce0c6b6d64d5b
Miguel Ocque <mocque@bu.edu>
2025-10-30 21:14:43 -0400

    Modified the patient class to have more associations,imports, and proper SpringBoot functions

--- UnifiedPatientManager/src/main/java/com/Eges411Team/UnifiedPatientManager/Patient.java
+++ UnifiedPatientManager/src/main/java/com/Eges411Team/UnifiedPatientManager/Patient.java
@@ -1,18 +1,63 @@
 package com.Eges411Team.UnifiedPatientManager;
 
+// this allows for database mapping
+import jakarta.persistence.*;
+
+// use the built in Date class and List class
+import java.util.Date;
+import java.util.List;
+
+// entity annotation to map this class to a database table
+@Entity
+// gives the database table a name 
+@Table(name = "patients")
 public class Patient {
-    // fields TODO
-    private String id;
+    // fields
+
+    // our primary key will be the patient ID
+    // denotes this field as the primary key
+    @Id
+    // specifies that the value will be generated automatically
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // we must give the column a name
+    @Column(name = "patient_id")
+    private Integer id;
+
+    // SpringBoot automatically creates names for the other columns
+    // the following are fields of a 'patient' with no relationships
     private String name;
-    private int age;
+    private Integer age;
     private String gender;
-    private String dateOfBirth;
+    private Date dateOfBirth;
     private String address;
     private String phoneNumber;
 
+    // the following fields are relationships to other classes
+
+    // a patient has one patient record
+    @OneToOne(mappedBy = "patient", cascade = CascadeType.ALL)
+    private PatientRecord patientRecord;
+
+    // a patient can have many allergies
+    @OneToMany(mappedBy = "patient", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private List<Allergy> allergies;
+
+    // and many medications
+    @OneToMany(mappedBy = "patient", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private List<Medication> medications;
+
+    // and prescriptions
+    @OneToMany(mappedBy = "patient", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private List<Prescription> prescriptions;  
+
+
+    // empty constructor required by JPA
+    public Patient() {
+        // no params
+    }
+
     // create the Constructor
-    public Patient(String id, String name, int age, String gender, String dateOfBirth, String address, String phoneNumber) {
-        this.id = id;
+    public Patient(String name, Integer age, String gender, Date dateOfBirth, String address, String phoneNumber) {
         this.name = name;
         this.age = age;
         this.gender = gender;
@@ -86,6 +131,38 @@ public class Patient {
 
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
+    }
+
+    // getter and setter for patient record
+    public PatientRecord getPatientRecord() {
+        return patientRecord;
+    }
+
+    public void setPatientRecord(PatientRecord patientRecord) {
+        this.patientRecord = patientRecord;
+    }
+
+    // getter and setter for allergies
+    public List<Allergy> getAllergies() {
+        return allergies;
+    }
+    public void setAllergies(List<Allergy> allergies) {
+        this.allergies = allergies;
+    }
+
+    // getter and setter for medications
+    public List<Medication> getMedications() {
+        return medications;
+    }
+    public void setMedications(List<Medication> medications) {
+        this.medications = medications;
+    }
+    // getter and setter for prescriptions
+    public List<Prescription> getPrescriptions() {
+        return prescriptions;
+    }
+    public void setPrescriptions(List<Prescription> prescriptions) {
+        this.prescriptions = prescriptions;
     }
 
 }
