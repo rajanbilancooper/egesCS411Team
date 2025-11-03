fb177fb279e30c7dc284182b7552f3263579537b
Miguel Ocque <mocque@bu.edu>
2025-11-03 12:26:03 -0500

    added correct tags for the patientController class

--- UnifiedPatientManager/src/main/java/com/Eges411Team/UnifiedPatientManager/controller/PatientController.java
+++ UnifiedPatientManager/src/main/java/com/Eges411Team/UnifiedPatientManager/controller/PatientController.java
@@ -1,10 +1,15 @@
 package com.Eges411Team.UnifiedPatientManager.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.persistence.*;
 
-@Controller
+// tells us this is a controller class
+@RestController
+// base path for all patient-related requests
+@RequestMapping("/patients")
 public class PatientController {
     
     // the controller has no attributes for now
