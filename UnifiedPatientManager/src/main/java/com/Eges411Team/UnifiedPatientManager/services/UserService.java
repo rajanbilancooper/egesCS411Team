9b2aa299755bee82ce7198f6364da9bf9d6694cf
Miguel Ocque <mocque@bu.edu>
2025-11-09 18:38:23 -0500

    Added methods for use cases that require retrieving a user from database and saving a user to the database

--- UnifiedPatientManager/src/main/java/com/Eges411Team/UnifiedPatientManager/services/UserService.java
+++ UnifiedPatientManager/src/main/java/com/Eges411Team/UnifiedPatientManager/services/UserService.java
@@ -11,19 +11,43 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 @Service
 public class UserService {
+    
     @Autowired
+    // dependency injection of UserRepository - means we can use its methods here
     private UserRepository userRepository;
 
     @Autowired
+    // dependency injection of UserSessionRepository - means we can use its methods here
     private UserSessionRepository userSessionRepository;
 
+
+    // *********** use case methods for retrieving a user from the database ******************
+
+    // finds a user by their ID
     public Optional<User> getUserById(Long id) {
         return userRepository.findById(id);
     }
 
-    // first must implement createUserSession in UserSessionRepository
+    // finds a user by their username
+    public Optional<User> getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    // *********************************************************************************
+
     
     // public UserSession createUserSession(User user) {
         
     // }
+
+    // *********** use case methods for saving a user to the database ******************
+    
+    // save a user to the database - only method needed for storing a new user
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    // *********************************************************************************
+
+
 }
