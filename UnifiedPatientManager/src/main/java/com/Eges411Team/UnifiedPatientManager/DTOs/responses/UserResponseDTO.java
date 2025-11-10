aa40e4ea0b2dc1e16c362e0c6c434c07bce485d2
Miguel Ocque <mocque@bu.edu>
2025-11-09 22:00:26 -0500

    userResponseDTO changes

---
+++ UnifiedPatientManager/src/main/java/com/Eges411Team/UnifiedPatientManager/DTOs/responses/UserResponseDTO.java
@@ -0,0 +1,40 @@
+package com.Eges411Team.UnifiedPatientManager.DTOs.responses;
+
+// Data Transfer Object for User responses
+public class UserResponseDTO {
+    private Long id;
+    private String username;
+    private String email;
+
+    // Constructor
+    public UserResponseDTO(Long id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
