f96abd665e9b83131a53ef1629089681bd2a42dc
Miguel Ocque <mocque@bu.edu>
2025-11-09 21:58:56 -0500

    Created a UserRequestDTO for when a new user is created to handle information in the controller class

---
+++ UnifiedPatientManager/src/main/java/com/Eges411Team/UnifiedPatientManager/DTOs/requests/UserRequestDTO.java
@@ -0,0 +1,44 @@
+package com.Eges411Team.UnifiedPatientManager.DTOs.requests;
+
+// Data Transfer Object for User requests
+public class UserRequestDTO {
+    private String username;
+    private String email;
+    private String password;
+
+    // Constructor
+    public UserRequestDTO() {
+        // Default constructor required by frameworks
+    }
+
+    public UserRequestDTO(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters and Setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
