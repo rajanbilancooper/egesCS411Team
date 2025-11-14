1554794e2919afcdc05e00f9f98590a5a038e05f
Miguel Ocque <mocque@bu.edu>
2025-11-13 22:04:13 -0500

    created DTOs in response and request for PatientRecord data transfer

--- UnifiedPatientManager/src/main/java/com/Eges411Team/UnifiedPatientManager/DTOs/responses/PatientRecordDTO.java
+++ UnifiedPatientManager/src/main/java/com/Eges411Team/UnifiedPatientManager/DTOs/responses/PatientRecordDTO.java
@@ -1,9 +1,33 @@
 package com.Eges411Team.UnifiedPatientManager.DTOs.responses;
+// imports
+import java.util.List;
+import java.util.Set;
+
+
+import com.Eges411Team.UnifiedPatientManager.entity.Allergy;
+import com.Eges411Team.UnifiedPatientManager.entity.Medication;
 
 
 // a patient record DTO to encapsulate all relevant patient record information
 // omits the need for a patientRecord entity
-
 public class PatientRecordDTO {
     
+    // field for patient ID
+    private Long patientId;
+
+    // first name field
+    private String firstName;
+
+    // last name field
+    private String lastName;
+
+    // email field
+    private String email;
+
+    // field for list of allergies
+    private Set<Allergy> allergies;
+
+    // field for list of medications
+    private List<Medication> medications;
+
 }
