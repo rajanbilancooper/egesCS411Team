2d4cc53a23b4c3cdc6570060403a808b27629390
Miguel Ocque <mocque@bu.edu>
2025-11-19 14:28:18 -0500

    added a path for the controller

--- UnifiedPatientManager/src/main/java/com/Eges411Team/UnifiedPatientManager/controller/PatientRecordController.java
+++ UnifiedPatientManager/src/main/java/com/Eges411Team/UnifiedPatientManager/controller/PatientRecordController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Eges411Team.UnifiedPatientManager.DTOs.requests.PatientRecordUpdateDTO;
@@ -15,6 +16,7 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 // controller for patient record related endpoints
 @RestController
+@RequestMapping("/api/patients")
 public class PatientRecordController {
     // dependency injection of the patientRecord Service
     @Autowired
