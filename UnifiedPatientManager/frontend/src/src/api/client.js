b48cfeaf8558caab63e233cf01b104746177829b
rhea <rthakur@bu.edu>
2025-11-19 14:17:54 -0500

    Merging

--- UnifiedPatientManager/frontend/src/src/api/client.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // e.g. http://localhost:8080
-  withCredentials: false,               // set true if using cookies/sessions
-});
